47261b750dcfe92797f4cac3fd78c8d1a5dc5337
YahyaAltintop <yahyaltntp@gmail.com>
2022-03-01 12:27:49 +0300

    delete unused files

--- test/testclient.js
+++
@@ -1,3 +0,0 @@
-import { Guid } from "../src/guid";
-
-console.log(Guid)
--- test/testnode.js
+++
@@ -1,5 +0,0 @@
-
-let { Guid } = require("../src/guid")
-
-console.log(Guid.NewGuid())
-// 1f6L3p1T-8A0I-8H9I-4h1f-4C8Y1G8k5x1e
